3c38cb89f619aa6dced421d584376461cc00020a
Lourdes Gonzalez <lougonzalez@itba.edu.ar>
2024-04-19 18:03:41 -0300

    Agregado archivo de ejemplo

---
+++ cl_ecg_net/util.py
@@ -0,0 +1,8 @@
+def config():
+    config = {
+        "batch_size": 32,
+        "train": 'C:/Users/lourd/Documents/Proyecto Final/pfc.venv/Scripts/cl_ecg_net/imagenes/train',
+        "dev": 'C:/Users/lourd/Documents/Proyecto Final/pfc.venv/Scripts/cl_ecg_net/imagenes/val',
+        "save_dir": 'C:/Users/lourd/Documents/Proyecto Final/pfc.venv/data/saved_ecg/'
+    }
+    return config
