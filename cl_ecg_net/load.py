386b4e680565d350cb33a7b2ba0725d4a54c5526
LourdesBranchi <116569743+LourdesBranchi@users.noreply.github.com>
2024-05-21 13:22:57 -0300

    Update load.py

--- cl_ecg_net/load.py
+++ cl_ecg_net/load.py
@@ -70,7 +70,7 @@ def load_dataset(data_json):
         data = [json.loads(l) for l in fid]
     for d in tqdm.tqdm(data):
         archivo = d['ecg'].split('Proyecto')[-1]
-        ecg_path = '/home/malperalta/Proyecto-Final'+archivo
+        ecg_path = '/home/lougonzalez/Proyecto-Final'+archivo
         print(f'Cargando el archivo ecg_path')
         labels.append(d['label'])
         ecgs.append(load_ecg(ecg_path))
