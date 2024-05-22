d65973be08281a689d70e82545b2d228f17615c5
LourdesBranchi <116569743+LourdesBranchi@users.noreply.github.com>
2024-05-22 13:17:17 -0300

    Update load.py

--- cl_ecg_net/load.py
+++ cl_ecg_net/load.py
@@ -71,14 +71,12 @@ def load_dataset(data_json):
     for d in tqdm.tqdm(data):
         archivo = d['ecg'].split('Proyecto')[-1]
         ecg_path = '/home/lougonzalez/Proyecto-Final'+archivo
-        print(f'Cargando el archivo ecg_path')
         labels.append(d['label'])
         ecgs.append(load_ecg(ecg_path))
     return ecgs, labels
 
 
 def load_ecg(record):
-    print(record)
     ecg = sio.loadmat(record)['ecg'].squeeze()
     ecg = signal.resample(ecg, 2048)
     return ecg
