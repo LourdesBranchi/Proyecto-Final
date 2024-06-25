44809b7f9180862b59997e88b6ad1cd181e0f9c9
LourdesBranchi <116569743+LourdesBranchi@users.noreply.github.com>
2024-06-25 11:46:05 -0300

    Update util.py

--- cl_ecg_net/util.py
+++ cl_ecg_net/util.py
@@ -3,6 +3,6 @@ def config():
         "batch_size": 32,
         "train": '/home/lougonzalez/pfc.venv/bin/Proyecto-Final/train',
         "dev": '/home/lougonzalez/pfc.venv/bin/Proyecto-Final/val',
-        "save_dir": '/home/lougonzalez/Proyecto-Final/data/saved_ecg/'
+        "save_dir": '/home/lougonzalez/pfc.venv/bin/Proyecto-Final/data/saved_ecg/'
     }
     return config
