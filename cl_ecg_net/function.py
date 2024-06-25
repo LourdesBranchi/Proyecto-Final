8b5ebbbf08f33d09077e28bf98a57f5016d60b81
LourdesBranchi <116569743+LourdesBranchi@users.noreply.github.com>
2024-06-25 11:31:22 -0300

    Update function.py

--- cl_ecg_net/function.py
+++ cl_ecg_net/function.py
@@ -29,24 +29,39 @@ class LossHistory(keras.callbacks.Callback):
         loss_epoch = np.array(self.loss['epoch'])
         acc_batch = np.array(self.acc['batch'])
         acc_epoch = np.array(self.acc['epoch'])
-
         val_loss_epoch = np.array(self.val_loss['epoch'])
-
         val_acc_epoch = np.array(self.val_acc['epoch'])
-
-        # Debugging: Print the values of the variables to be saved
-        print("loss_batch:", loss_batch)
-        print("loss_epoch:", loss_epoch)
-        print("acc_batch:", acc_batch)
-        print("acc_epoch:", acc_epoch)
-        print("val_loss_epoch:", val_loss_epoch)
-        print("val_acc_epoch:", val_acc_epoch)
         # Check for None values and handle them
         if loss_batch is None or loss_epoch is None or acc_batch is None or acc_epoch is None:
             raise ValueError("One or more variables to be saved are None.")
 
         scio.savemat(path, {'loss_batch': loss_batch,'loss_epoch':loss_epoch,'acc_batch':acc_batch,'acc_epoch':acc_epoch,
                             'val_loss_epoch':val_loss_epoch, 'val_acc_epoch':val_acc_epoch})
+    def plot_loss_and_accuracy(self, save_dir):
+        training_loss = self.loss['epoch']
+        validation_loss = self.val_loss['epoch']
+        training_accuracy = self.acc['epoch']
+        validation_accuracy = self.val_acc['epoch']
+        
+        # Crear el gráfico de pérdidas
+        plt.plot(training_loss, label='Training Loss')
+        plt.plot(validation_loss, label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Loss')
+        plt.legend()
+        plt.savefig(save_dir + 'loss_plot.png')
+        plt.close()
+
+        # Crear el gráfico de precisiones
+        plt.plot(training_accuracy, label='Training Accuracy')
+        plt.plot(validation_accuracy, label='Validation Accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.title('Training and Validation Accuracy')
+        plt.legend()
+        plt.savefig(save_dir + 'accuracy_plot.png')
+        plt.close()
 
 
 def specificity(y_true,y_test):
@@ -62,4 +77,18 @@ def specificity(y_true,y_test):
 def cal_auc(y,y_pre):
     fpr, tpr, thresholds = roc_curve(y, y_pre, pos_label=None, sample_weight=None,drop_intermediate=True)
     auc_area = auc(fpr, tpr)
+    
+    # Plot the ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % auc_area)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig(save_path + 'roc_curve.png')
+    plt.close()   
+    
     return auc_area
